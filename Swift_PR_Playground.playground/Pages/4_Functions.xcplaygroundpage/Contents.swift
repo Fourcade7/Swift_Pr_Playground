b996de75eefd51abbb241253f0b4735daacd621c
Fourcade7 <fourcadeaminov7@gmail.com>
2022-12-29 17:04:40 +0500

    Functions end

--- Swift_PR_Playground.playground/Pages/4_Functions.xcplaygroundpage/Contents.swift
+++ Swift_PR_Playground.playground/Pages/4_Functions.xcplaygroundpage/Contents.swift
@@ -9,8 +9,16 @@ import Foundation
 func sayhello(){
     print("Assalomu Aleykum")
 }
+func addTwoNumber(a:Int,b:Int)->Int{
+    
+    
+    return 7
+}
 
 
+
+let a=addTwoNumber(a: 5, b: 9)
+print(a)
 
 sayhello()
 
