776f0c09e03ad6f754acee557ef71ec4cc795efd
Fourcade7 <fourcadeaminov7@gmail.com>
2023-01-02 10:16:18 +0500

    class and structs

---
+++ Swift_PR_Playground.playground/Pages/5_Class_and_Structs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+
+
+class Google{
+    
+    var name:String=""
+    var age:Int=0
+    
+    func youtube(){
+        print("\(age) Video is playing")
+    }
+}
+
+let google=Google()
+google.age=24
+
+print(google.age)
+google.youtube()
+
+
+
+class Yandex{
+    init(){
+        print("this is init block")
+    }
+}
+
+let yandex=Yandex()
+
+
+class Zoom{
+    var name:String
+    init(name:String){
+        self.name=name
+    }
+}
+
+let zoom=Zoom(name: "Fourcade")
+print(zoom.name)
+
+
+
+
+struct Tecno{
+    let name:String
+    let model:Int
+    init(name:String,model:Int){
+        self.name=name
+        self.model=model
+    }
+    
+    
+    func printinfo(){
+        print("\(name)\n\(model)")
+    }
+    
+}
+
+let tecno=Tecno(name: "Tecno Pova Neo 2", model: 6)
+tecno.printinfo()
