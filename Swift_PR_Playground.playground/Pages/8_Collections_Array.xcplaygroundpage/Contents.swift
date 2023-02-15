dd69a411774edab37693addc0c60a532161ed296
Fourcade7 <fourcadeaminov7@gmail.com>
2023-02-15 21:28:28 +0500

    array objects

--- Swift_PR_Playground.playground/Pages/8_Collections_Array.xcplaygroundpage/Contents.swift
+++ Swift_PR_Playground.playground/Pages/8_Collections_Array.xcplaygroundpage/Contents.swift
@@ -2,6 +2,85 @@
 
 import Foundation
 
-var greeting = "UAxa axaxaxaxaxaxaxxaxax"
+
+//yangi array elon qilish
+var  intarray=[Int]()
+
+//element qoshish
+intarray.append(7)
+intarray.append(10)
+intarray.append(40)
+intarray.append(0)
+intarray.append(3)
+intarray.append(70)
+
+
+//bir dona elementni ishlatish
+print(intarray[2])
+
+//arrayning uzunligi
+print(intarray.count)
+var a=intarray.count+1
+
+//hamma elementini chiqarish
+for i in 0...intarray.count-1 {
+    print(intarray[i])
+}
+//hamma elementini chiqarish
+print("Lyuboy")
+for lyuboy in intarray{
+    print(lyuboy)
+}
+
+
+print("Arrays String types")
+
+var arraystring=[String]()
+
+arraystring.append("Google")    //0
+arraystring.append("Youtube")  //1
+arraystring.append("Yandex")    //2
+//elemetni o`chirish
+arraystring.remove(at: 1)
+//hammasini o`chirish
+//arraystring.removeAll()
+
+var c=arraystring.count
+print(c)
+
+for i in 0...arraystring.count-1{
+    print(arraystring[i])
+}
+
+
+
+class Mirjalol2{
+    init(name:String,phone:String,car:String){
+        
+    }
+}
+struct Mirjalol{
+    var name:String
+    var phone:String
+    var car:String
+}
+
+var marray=[Mirjalol]()
+
+var ml=Mirjalol(name: "M777", phone: "Red", car: "Cobalt")
+
+marray.append(Mirjalol(name: "Mirjalol", phone: "Iphone", car: "Ferrari"))
+marray.append(Mirjalol(name: "Mirjalol", phone: "Redmi", car: "Lomborghini"))
+marray.append(Mirjalol(name: "Mirjalol", phone: "Iphone", car: "Cobalt"))
+
+
+print(marray[1].phone)
+
+
+
+
+
+
+
 
 
